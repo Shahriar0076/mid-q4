5720b11eac98f1348428ed6315326566e610d636
Shahriar0076 <47733699+Shahriar0076@users.noreply.github.com>
2019-10-30 09:46:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 /* Add your script here */
+
+//Task 1
+$('#list').click(function(){
+
+	$.ajax({
+		method:'GET',
+		url:'http://skeleton.mernbook.com/api/users' ,
+
+
+
+	}).done(function(response){
+		response.forEach(element => {
+			let user = $('<p>').text(element.name)
+			$('#users').append(user)
+
+		}).fail(function(response){
+			console.log(response)
+		})
+	})
+
+})
+
+//Task 2
+$('#add').click(function(){
+
+	let user = {name:'',email:'',password:''}
+	user.name = $('#name').val
+	user.email   = $('#email').val
+	user.password  = $('#password').val
+
+	$.ajax({
+		method:'POST',
+		url:'http://skeleton.mernbook.com/api/users' ,
+		data : user
+		}).done(function(response){
+
+		$('#name').val('')
+		$('#name').val('')
+		$('#name').val('')
+		$('#error').TEXT('User Created')
+		console.log("done")
+
+	}).fail(function(response){
+		$('#error').text(response.responseJSON.error)
+		console.log("failed")
+	})
+
+
+
+})
